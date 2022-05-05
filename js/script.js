d5846f14900e56de5a26c835cc4e5c62a21f5841
ArtemUholkov <artem.ugolkov@ukr.net>
2022-05-05 13:05:03 +0300

    animation burger created

--- js/script.js
+++ js/script.js
@@ -5,7 +5,18 @@ const popupSlide = document.querySelector("#popupSlide")
 burger.addEventListener("click", burgerHandler);
 
 function burgerHandler(e) {
-    e.preventDefault();
-    popup.classList.toggle("open");
-    popupSlide.classList.toggle("slidein")
+    if (popup.classList.contains('open')) {
+        popup.classList.add('close')
+        popupSlide.classList.add('slideout');
+        setTimeout(() => {
+            popup.classList.remove('open');
+            popup.classList.remove('close');
+            popupSlide.classList.remove('slideout');
+        }, 300);
+
+    } else {
+        e.preventDefault();
+        popup.classList.add("open");
+        popupSlide.classList.add("slidein")
+    }
 }
