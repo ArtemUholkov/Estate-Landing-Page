21badce3e024457edf7e9c276b39508da77910d8
ArtemUholkov <artem.ugolkov@ukr.net>
2022-05-05 17:31:35 +0300

    header ready

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ const popupSlide = document.querySelector("#popupSlide")
 burger.addEventListener("click", burgerHandler);
 
 function burgerHandler(e) {
+
     if (popup.classList.contains('open')) {
         popup.classList.add('close')
         popupSlide.classList.add('slideout');
@@ -19,5 +20,7 @@ function burgerHandler(e) {
 
         popup.classList.add("open");
         popupSlide.classList.add("slidein")
+
     }
+    burger.classList.toggle('active');
 }
