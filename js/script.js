63eedc046fa54503405f2edd933326fddfb6740c
ArtemUholkov <artem.ugolkov@ukr.net>
2022-05-05 12:05:30 +0300

    burger slider created

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 const burger = document.querySelector("#burger");
 const popup = document.querySelector("#popup");
+const popupSlide = document.querySelector("#popupSlide")
 
 burger.addEventListener("click", burgerHandler);
 
 function burgerHandler(e) {
     e.preventDefault();
     popup.classList.toggle("open");
+    popupSlide.classList.toggle("slidein")
 }
