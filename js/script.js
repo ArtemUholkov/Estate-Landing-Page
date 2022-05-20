c29108346a97f6d8c457fc909b0d096b2c082ea8
ArtemUholkov <artem.ugolkov@ukr.net>
2022-05-20 10:16:44 +0300

    slider swiper ready 100%

--- js/script.js
+++ js/script.js
@@ -148,24 +148,3 @@ prevButton.addEventListener("click", function () {
 // pixelCounter = sliderWidth;
 // sliderWrapper.style.left = -pixelCounter + 'px';
 
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'vertical',
-    loop: true,
-
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination',
-    },
-
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-
-    // And if we need scrollbar
-    scrollbar: {
-        el: '.swiper-scrollbar',
-    },
-});
