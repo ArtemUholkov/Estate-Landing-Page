c797a246d945b0eb277ae2cba7a57e7b0b104ece
ArtemUholkov <artem.ugolkov@ukr.net>
2022-05-23 10:42:03 +0300

    popup added to current listings section

--- js/script.js
+++ js/script.js
@@ -148,3 +148,30 @@ prevButton.addEventListener("click", function () {
 // pixelCounter = sliderWidth;
 // sliderWrapper.style.left = -pixelCounter + 'px';
 
+
+
+// Get the modal
+var modal = document.querySelector("#myModal");
+var modalImg = document.querySelector("#img01");
+var captionText = document.querySelector("#caption");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.querySelectorAll(".current_listings_img");
+console.log(img);
+
+img.forEach(el => {
+    el.onclick = function () {
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+    }
+});
+
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+modal.onclick = function () {
+    modal.style.display = "none";
+}
